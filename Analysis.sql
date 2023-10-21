e25d75cb195b9f003d31c9a4158c65849d0e8fea
Sambify <148338768+Sambify@users.noreply.github.com>
2023-10-21 23:16:05 +0530

    Update Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -9,11 +9,55 @@ SELECT
   ELSE
   'NO'
 END
-  AS Pis,
-  COUNT(brand) AS DSJ
+  AS Category,
+  COUNT(brand) AS Number
 FROM
   `valid-heuristic-371516.MobilePhones.Mobile`
 GROUP BY
   1
+
+Results -
+
+[{
+  "Category": "NO",
+  "Number": "1941"
+}, {
+  "Category": "OK",
+  "Number": "1659"
+}]
+
+
+
+  
+
+Number of display's
+
+SELECT
+  display,
+  AVG(price____)AS average_price
+FROM
+  `valid-heuristic-371516.MobilePhones.Mobile`
+GROUP BY
+  display
+HAVING
+  AVG(price____) > 1000
+  
+
+Results - 
+  
+
+ [{
+  "display": null,
+  "average_price": "2760.121908127212"
+}, {
+  "display": "IPS LCD",
+  "average_price": "1563.9023622047259"
+}, 
+
+
+
+
+
+
 
 
